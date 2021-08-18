b97d2649e3f043bf878d8da20923b9943879e89e
TIMM SNOW <two.tone.snow@gmail.com>
2021-08-18 12:49:22 -0400

    adds multi-app demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <label class="typo__label">Simple select / dropdown</label>
+    <multiselect
+      v-model="value"
+      :options="options"
+      :multiple="true"
+      :close-on-select="false"
+      :clear-on-select="false"
+      :preserve-search="true"
+      placeholder="Pick some"
+      label="name"
+      track-by="name"
+      :preselect-first="true"
+    >
+      <template slot="selection" slot-scope="{ values, search, isOpen }">
+        <span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">
+          {{ values.length }} options selected
+        </span>
+      </template>
+    </multiselect>
+    <!-- <pre class="language-json"><code>{{ value  }}</code></pre> -->
+    <!-- {{ value }} -->
+    <button v-on:click="postIt()">BUTTONS</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Multiselect from "vue-multiselect";
+import axios from "axios";
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld,
+    Multiselect,
+  },
+  data() {
+    return {
+      value: [],
+      options: [],
+    };
+  },
+  created: function () {
+    this.getJobs();
+  },
+  methods: {
+    getJobs: function () {
+      axios.get("http://localhost:3000/api/users").then((response) => {
+        response.data.forEach((item) => {
+          console.log(item);
+          var object = {};
+          object["name"] = item.name;
+          object["id"] = item.id;
+          this.options.push(object);
+        });
+        console.log(this.options);
+      });
+    },
+    postIt: function () {
+      console.log(this.value);
+    },
   },
 };
 </script>
